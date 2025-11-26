418b43cd7096864fe52a0ff5e1e027c5372afe05
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-11-27 00:20:54 +0600

    updated .\next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,24 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: ["flowbite.s3.amazonaws.com"],
+//      // allow external images from Flowbite
+//   },
+// };
+
+// module.exports = nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["flowbite.s3.amazonaws.com"],
-     // allow external images from Flowbite
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "flowbite.s3.amazonaws.com",
+        // optional: you can add pathname if you only want specific folders
+        // pathname: '/path/to/images/**',
+      },
+    ],
   },
 };
 
